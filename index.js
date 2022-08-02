a65f16c67ca6e605f05c62a0f3e554952a3ba47c
KimDzim <dzim0620@gmail.com>
2022-08-02 16:13:21 +0900

    day2 mission4

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function hamsu(){
+export function hamsu(){
     const leehun = 'tlqkftoRl';
     return leehun;
 }
-console.log(hamsu());
+
---
+++ mission.js
@@ -0,0 +1,6 @@
+import {hamsu} from './index'
+function beounsu(){
+    const gg = 'agree';
+    return gg;
+}
+console.log(hamsu()+beounsu())
